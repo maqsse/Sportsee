0ab7a35aa0540507f561504b82713999af18c3f5
max <32159805+maqsse@users.noreply.github.com>
2023-04-21 14:15:27 +0200

    modif

--- src/Components/Error.js
+++ src/Components/Error.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from "react-router-dom";
-import Upbar from '../components/Upbar'
-import Sidebar from '../components/Sidebar'
+import Upbar from './Upbar'
+import Sidebar from './Sidebar'
 import '../styles/upbar.css'
 import '../styles/sidebar.css'
 import '../styles/error.css'
@@ -11,7 +11,7 @@ const Error = () => {
     <div>
       <Upbar/>
     <article className='error'>
-      <h1> 403 </h1>
+      <h1> 404 </h1>
       <h2> Oups! La page que vous demandez n'existe pas </h2>
       <Link to="/user/12">Retour à la page d'accueil</Link>
       <Sidebar/>
